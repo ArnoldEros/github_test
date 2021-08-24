4b45eca272d2d06934aa2e2145aa45fa2de4a734
EROS <1874863206@qq.com>
2021-08-24 19:50:20 +0800

    第二次提交

---
+++ src/test/java/com/eros/test/UtilTest.java
@@ -0,0 +1,26 @@
+package com.eros.test;
+
+import com.eros.util.Sort;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UtilTest {
+
+    @Test
+    public void testSort(){
+        List<Integer> list=new ArrayList<>();
+        list.add(4);
+        list.add(2);
+        list.add(5);
+        list.add(1);
+        list.add(6);
+        list.add(10);
+        list.add(3);
+        List<Integer> sortList = Sort.BubbleSort(list);
+        for (Integer integer : list) {
+            System.out.println(integer);
+        }
+    }
+}
